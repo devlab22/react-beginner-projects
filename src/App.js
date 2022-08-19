9ba5fdff560da337ceb54272fca00b445a815135
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2022-08-19 12:18:17 +0200

    end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+
+  const [counter, setCounter] = React.useState(0);
+
+  const handleOnPlus = () => {
+    setCounter(counter + 1)
+  }
+
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{counter}</h1>
+        <button className="minus" onClick={() => setCounter(counter - 1)}>- Минус</button>
+        <button className="plus" onClick={handleOnPlus} >Плюс +</button>
       </div>
     </div>
   );
