688cb20d3e801817be80f7c483775361e6c25868
devlab22 <v.engelhard@gmx.net>
2022-08-25 10:34:42 +0200

    users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+import axios from 'axios';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const [invites, setInvites] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  const getUsers = async () => {
+
+    setIsLoading(true)
+    try {
+
+      const res = await axios.get('https://reqres.in/api/users');
+
+      setUsers(res.data.data)
+
+    }
+    catch (err) { console.log(err) }
+    finally {
+      setIsLoading(false)
+    }
+  }
+
+  React.useEffect(() => {
+
+    getUsers()
+
+  }, [])
+
+
+  const OnChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const OnInvite = (id) => {
+
+    if (invites.includes(id)) {
+      setInvites(prev => prev.filter(_id => _id !== id))
+    } else {
+      setInvites(prev => [...prev, id])
+    }
+
+  }
+
+  const OnInviteUsers = () => {
+    setSuccess(true);
+  }
+
+  const OnClickBack = () => {
+    setSuccess(false)
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+
+      {
+        success ? (
+          <Success
+            count={invites.length}
+            OnClickBack={OnClickBack}
+          />
+        ) : (
+          <Users
+            items={users}
+            isLoading={isLoading}
+            searchValue={searchValue}
+            OnChangeSearchValue={OnChangeSearchValue}
+            OnClickInvite={OnInvite}
+            invites={invites}
+            OnInviteUsers={OnInviteUsers}
+          />
+        )
+      }
+
     </div>
   );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export const Success = ({ count }) => {
+export const Success = ({ count, OnClickBack }) => {
   return (
-    <div class="success-block">
+    <div className="success-block">
       <img src="/assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <button onClick={OnClickBack} className="send-invite-btn">Назад</button>
     </div>
   );
 };
